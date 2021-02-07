1e9dd364a2e15a144c0a387ec597535867a582f7
Paul Gareze <paul.gareze@gmail.com>
2021-02-07 20:51:35 +0000

    Update import for assertThat
     - Remove use of deprecated import.

--- java/challenge_2/src/test/java/com/ministryoftesting/Challenge2Tests.java
+++ java/challenge_2/src/test/java/com/ministryoftesting/Challenge2Tests.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 //    Welcome to UI Automation Challenge 2
 //
